8c819614103788e5961c66eaeeb763449807cb0a
João Faria <joao.faria@unige.ch>
2024-12-19 22:58:38 +0100

    cleanup package

--- authors/__init__.py
+++ authors/__init__.py
@@ -1,14 +1,15 @@
 # flake8: noqa
-from importlib.metadata import version, PackageNotFoundError
+__all__ = ['Authors']
+
+from importlib.metadata import version as _version, PackageNotFoundError
 
 try:
-    __version__ = version("authors")
-except PackageNotFoundError:
-    # package is not installed
+    __version__ = _version("authors")
+except PackageNotFoundError: # package is not installed
     pass
 
-from .authors import Authors, delete_author
 
+from .authors import Authors
 from .authors import register_author, delete_author
 from .authors import (update_author_name, update_author_affiliations,
                       update_author_email, change_affiliation,
