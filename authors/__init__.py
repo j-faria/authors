fffe3c8a4f755e6bf0a6a7ef7bde2239143c0521
João Faria <joao.faria@unige.ch>
2024-12-19 15:41:00 +0100

    expose __version__

--- authors/__init__.py
+++ authors/__init__.py
@@ -1,4 +1,11 @@
 # flake8: noqa
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("authors")
+except PackageNotFoundError:
+    # package is not installed
+    pass
 
 from .authors import Authors, delete_author
 
