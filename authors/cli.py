dfaa0a121d6af272cb349e0a385da8ec28d5f1f4
João Faria <joao.faria@unige.ch>
2024-12-19 21:33:28 +0100

    new cli scripts

--- authors/cli.py
+++ authors/cli.py
@@ -1,5 +1,14 @@
 from argparse import ArgumentParser
-from .authors import update_author_email
+from .authors import delete_author, update_author_email, update_author_name, update_author_orcid
+
+
+def cli_update_author_name():
+    doc = update_author_name.__doc__.split('\n')[0]
+    parser = ArgumentParser(description=doc)
+    parser.add_argument('old_name', type=str)
+    parser.add_argument('new_name', type=str)
+    args = parser.parse_args()
+    update_author_name(args.old_name, args.new_name)
 
 
 def cli_update_author_email():
@@ -10,3 +19,19 @@ def cli_update_author_email():
     args = parser.parse_args()
     update_author_email(args.author, args.email)
 
+
+def cli_update_author_orcid():
+    doc = update_author_orcid.__doc__.split('\n')[0]
+    parser = ArgumentParser(description=doc)
+    parser.add_argument('author', type=str)
+    parser.add_argument('orcid', type=str)
+    args = parser.parse_args()
+    update_author_orcid(args.author, args.orcid)
+
+
+def cli_delete_author():
+    doc = delete_author.__doc__.split('\n')[0]
+    parser = ArgumentParser(description=doc)
+    parser.add_argument('author', type=str)
+    args = parser.parse_args()
+    delete_author(args.author)
