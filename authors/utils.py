0175bbb0a7f3a611cffe84632848b499e35e9baa
João Faria <joao.faria@unige.ch>
2025-01-06 21:30:10 +0100

    closest author with bitap search; fix encoding errors

--- authors/utils.py
+++ authors/utils.py
@@ -2,6 +2,28 @@ import re
 from functools import lru_cache
 
 
+ENCODING_ERRORS = {
+    'Ã¡': 'á',
+    'â€™': "'",
+    'MarÃ­a': 'María',
+    'fÃ¼r': "für",
+    'KÃ¶nigstuhl': 'Königstuhl',
+    'IngenierÃ­a': 'Ingeniería',
+    'IbÃ¡Ã±ez': 'Ibáñez',
+    'PeÃ±alolÃ©n': 'Peñalolén'
+}
+
+
+def argmin(seq):
+    return min(range(len(seq)), key=lambda x : seq[x])
+
+def argmax(seq):    
+    return max(range(len(seq)), key=lambda x : seq[x])
+
+def argsort(seq):
+    return sorted(range(len(seq)), key=seq.__getitem__)
+
+
 def lev_dist(a: str, b: str) -> float:
     """
     Calculates the Levenshtein distance between two input strings `a` and `b`
@@ -30,6 +52,92 @@ def lev_dist(a: str, b: str) -> float:
             min_dist(s1 + 1, s2 + 1),  # replace character
         )
     return min_dist(0, 0)
+
+
+def bitapSearch(haystack, needle, maxErrors):
+    """Bitap (Shift-Or) fuzzy searching algorithm with Wu-Manber modifications.
+    http://habrahabr.ru/post/114997/
+    http://habrahabr.ru/post/132128/
+    http://ru.wikipedia.org/wiki/Двоичный_алгоритм_поиска_подстроки
+
+    Search needle(pattern) in haystack(real word from text) with maximum alterations = maxErrors.
+    If maxErrors equal 0 - execute precise searching only.
+
+    Return approximately place of needle in haystack and number of alterations.
+    If needle can't find with maxErrors alterations, return tuple of empty string and -1.
+    """
+    haystackLen = len(haystack)
+    needleLen = len(needle)
+
+    def _generateAlphabet(needle, haystack):
+        """Genarating mask for each letter in haystack.
+        This mask shows presence letter in needle.
+        """
+        alphabet = {}
+        for letter in haystack:
+            if letter not in alphabet:
+                letterPositionInNeedle = 0
+                for symbol in needle:
+                    letterPositionInNeedle = letterPositionInNeedle << 1
+                    letterPositionInNeedle |= int(letter != symbol)
+                alphabet[letter] = letterPositionInNeedle
+        return alphabet
+
+    alphabet = _generateAlphabet(needle, haystack)
+
+    table = [] # first index - over k (errors count, numeration starts from 1), second - over columns (letters of haystack)
+    emptyColumn = (2 << (needleLen - 1)) - 1
+
+    #   Generate underground level of table
+    underground = []
+    [underground.append(emptyColumn) for i in range(haystackLen + 1)]
+    table.append(underground)
+    # _printTable(table[0], needleLen)
+
+    #   Execute precise matching
+    k = 1
+    table.append([emptyColumn])
+    for columnNum in range(1, haystackLen + 1):
+        prevColumn = (table[k][columnNum - 1]) >> 1
+        letterPattern = alphabet[haystack[columnNum - 1]]
+        curColumn = prevColumn | letterPattern
+        table[k].append(curColumn)
+        if (curColumn & 0x1) == 0:
+            place = haystack[columnNum - needleLen : columnNum]
+            return (place, k - 1)
+    # _printTable(table[k], needleLen)
+
+    #   Execute fuzzy searching with calculation Levenshtein distance
+    for k in range(2, maxErrors + 2):
+        # print("Errors =", k - 1)
+        table.append([emptyColumn])
+
+        for columnNum in range(1, haystackLen + 1):
+            prevColumn = (table[k][columnNum - 1]) >> 1
+            letterPattern = alphabet[haystack[columnNum - 1]]
+            curColumn = prevColumn | letterPattern
+            
+            insertColumn = curColumn & (table[k - 1][columnNum - 1])
+            deleteColumn = curColumn & (table[k - 1][columnNum] >> 1)
+            replaceColumn = curColumn & (table[k - 1][columnNum - 1] >> 1)
+            resColumn = insertColumn & deleteColumn & replaceColumn
+            
+            table[k].append(resColumn)
+            if (resColumn & 0x1) == 0:
+                startPos = max(0, columnNum - needleLen - 1) # taking in account Replace operation
+                endPos = min(columnNum + 1, haystackLen) # taking in account Replace operation
+                place = haystack[startPos : endPos]
+                return (place, k - 1)
+            
+        # _printTable(table[k], needleLen)
+    return ("", -1)
+
+
+def closest_author(author, authors, closest=1, distance='bitap'):
+    if distance == 'bitap':
+        d = [bitapSearch(a, author, len(a))[1] for a in authors]
+        return [authors[i] for i in argsort(d)[:closest]]
+
 
 def find_bracket_last_name(name):
     last_name = None
@@ -168,6 +276,13 @@ def substr_in_list(sub, lst):
             return True, i
     return False, None
 
+
+def fix_encoding_errors(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        text = f.read()
+    text = text_replace_dict(text, ENCODING_ERRORS)
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write(text)
 
 def humanize_yaml(file):
     import fileinput
